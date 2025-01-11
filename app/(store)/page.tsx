a9048fd6336da34ec441e629cea4d94b045d1a7e
Dinesh31946 <gosavidinesh54@gmail.com>
2025-01-11 21:12:19 +0530

    unused varibales commented

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -4,13 +4,13 @@ import { FeaturedProductCarousel } from "@/components/FeaturedProductCarousel";
 import ProductsView from "@/components/ProductsView";
 // import { Button } from "@/components/ui/button";
 import { getAllCategories } from "@/sanity/lib/products/getAllCetgories";
-import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+// import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import { getFeaturedProducts } from "../api/getFeaturedPriducts/route";
 import NewArrivalSection from "@/components/NewArrival";
 import WavePromo from "@/components/WavePromo";
 
 export default async function Home() {
-    const products = await getAllProducts();
+    // const products = await getAllProducts();
     const categories = await getAllCategories();
     const featuredProducts = await getFeaturedProducts();
 
