606e1c21599647cf69773a9fa12dcc9449569443
Dinesh31946 <gosavidinesh54@gmail.com>
2025-01-11 20:50:45 +0530

    preoduct view component modified to complie with lint

--- components/ProductsView.tsx
+++ components/ProductsView.tsx
@@ -2,14 +2,16 @@ import { GetServerSideProps } from "next";
 // import ProductGrid from "./ProductGrid";
 import { CategorySelector } from "./CategorySelector";
 import { getAllCategories } from "@/sanity/lib/products/getAllCetgories"; // Importing the fetch function
-import { Category, Product } from "@/sanity.types";
+// import { Category, Product } from "@/sanity.types";
+import { Category } from "@/sanity.types";
 
 interface ProductsViewProps {
-    products: Product[];
+    // products: Product[];
     categories: Category[]; // Categories will be passed as props
 }
 
-const ProductsView = ({ products, categories }: ProductsViewProps) => {
+// const ProductsView = ({ products, categories }: ProductsViewProps) => {
+const ProductsView = ({ categories }: ProductsViewProps) => {
     return (
         <div className="flex flex-col">
             {/* Categories */}
