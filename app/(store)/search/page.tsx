fa74ed27c73e4f9a3a52acaa5c80b1a19a6d89ad
Dinesh31946 <gosavidinesh54@gmail.com>
2025-01-15 14:40:28 +0530

    lint errors fixed

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,4 +1,4 @@
-import ProductGrid from "@/components/ProductGrid";
+import { ProductGrid } from "@/components/ProductGrid";
 import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 
 async function SearchPage({
